bcd7df8dc028e01f5886cdfcfbbae7d930899a2a
KDESP-LAPTOP\Konstantinos <kdesp2003@gmail.com>
2022-05-30 23:31:02 +0300

    I fucked up i think

--- docs/script.js
+++ docs/script.js
@@ -1,1 +1,34 @@
+window.onresize = function(event) {
+  document.location.reload(true);
+}
 
+var href = window.location.href.split("/")
+var html_location = href[href.length-1]
+
+if (window.innerWidth >= 960 && html_location == "index2.html") {
+    window.location = "index.html";
+}
+if (window.innerWidth >= 960 && html_location == "projects2.html") {
+    window.location = "projects.html";
+}
+if (window.innerWidth >= 960 && (html_location == "index2_gr.html") {
+    window.location = "index_gr.html";
+}
+if (window.innerWidth >= 960 && html_location == "projects2_gr.html") {
+    window.location = "projects_gr.html";
+}
+
+
+
+if (window.innerWidth < 960 && html_location == "index.html") {
+    window.location = "index2.html";
+}
+if (window.innerWidth < 960 && html_location == "projects.html") {
+    window.location = "projects2.html";
+}
+if (window.innerWidth < 960 && html_location == "index_gr.html") {
+    window.location = "index2_gr.html";
+}
+if (window.innerWidth < 960 && html_location == "projects_gr.html") {
+    window.location = "projects2_gr.html";
+}
