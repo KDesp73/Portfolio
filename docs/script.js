416f277d905ca33c5fc466f29b14765fa37b0d39
KDESP-LAPTOP\Konstantinos <kdesp2003@gmail.com>
2023-01-31 01:09:58 +0200

    New site

--- docs/script.js
+++ docs/script.js
@@ -1,34 +1,99 @@
-/*window.onresize = function(event) {
-  document.location.reload(true);
+
+function UrlExists() {
+  var url = window.location.href;
+  var http = new XMLHttpRequest();
+  http.open("HEAD", url, false);
+  http.send();
+  if (http.status != 404) return true;
+  else return false;
 }
 
-var href = window.location.href.split("/")
-var html_location = href[href.length-1]
+const fetch = require("node-fetch");
+fetch("https://httpstat.us/404")
+  .then(function () {
+    console.log("200 OK");
+  })
+  .catch(function () {
+    console.log("404 Not Found");
+    loadHTML();
+  });
 
-if (window.innerWidth >= 960 && html_location !== "index2.html") {
-    window.location = "index.html";
-}
-if (window.innerWidth >= 960 && html_location == "projects2.html") {
-    window.location = "projects.html";
-}
-if (window.innerWidth >= 960 && (html_location == "index2_gr.html") {
-    window.location = "index_gr.html";
-}
-if (window.innerWidth >= 960 && html_location == "projects2_gr.html") {
-    window.location = "projects_gr.html";
+function loadHTML() {
+  window.location.href = "index.html";
 }
 
+function toggleMenu() {
+  document.body.classList.toggle("open");
+}
+function toggleProject() {
+  document.body.classList.toggle("open-project");
+}
 
+function return_() {
+  document.getElementsByClassName("project").setAttribute("opacity", "0");
+}
 
-if (window.innerWidth < 960 && html_location == "index.html") {
-    window.location = "index2.html";
+function toHome() {
+  window.location.href =
+    "https://users.iee.ihu.gr/~iee2021035/my-portfolio/index.html";
 }
-if (window.innerWidth < 960 && html_location == "projects.html") {
-    window.location = "projects2.html";
+
+function toggleCalendar() {
+  var x = document.getElementById("calendar");
+  if (x.style.visibility === "hidden") {
+    x.style.visibility = "visible";
+  } else {
+    x.style.visibility = "hidden";
+  }
 }
-if (window.innerWidth < 960 && html_location == "index_gr.html") {
-    window.location = "index2_gr.html";
+
+function checkInput() {
+  const email = document.getElementById("email");
+  const fname = document.getElementById("firstname");
+  const lname = document.getElementById("lastname");
+  const message = document.getElementById("message");
+  const form = document.getElementById("form");
+  const sent = document.getElementById("sent");
+  const container = document.getElementById("container");
+  const container_sent = document.getElementById("container-sent");
+
+  if (
+    email.value == "" ||
+    fname.value == "" ||
+    lname.value == "" ||
+    message.value == ""
+  ) {
+    window.alert("Συμπληρώστε όλα τα κελία");
+  } else {
+    container.style.display = "none";
+
+    container_sent.style.display = "flex";
+
+    //window.alert("Το μήνυμα σας στάλθηκε με επιτυχία");
+
+    console.log(email.value);
+    console.log(fname.value);
+    console.log(lname.value);
+    console.log(message.value);
+  }
 }
-if (window.innerWidth < 960 && html_location == "projects_gr.html") {
-    window.location = "projects2_gr.html";
-}*/
+
+function isDone() {
+  return (
+    email.value != "" &&
+    fname.value != "" &&
+    lname.value != "" &&
+    message.value != ""
+  );
+}
+
+function copyToClipboard() {
+  navigator.clipboard.writeText("KDesp73#6998");
+  alert("Discord profile copied to clipboard");
+}
+
+function checkLength() {
+  const message = document.getElementById("message");
+  if (message.value.length > 100)
+    window.alert("You cannot submit this message./nOver 100 characters");
+}
