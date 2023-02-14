cf06485ad5b831d245dcdf183f72e064b942797d
Konstantinos Despoinidis <63654361+KDesp73@users.noreply.github.com>
2023-02-14 23:15:38 +0200

    Add files via upload

---
+++ skills.js
@@ -0,0 +1,11 @@
+const skills = [
+  { tag: "C", percentage: "50%" },
+  { tag: "Java", percentage: "80%" },
+  { tag: "Python", percentage: "70%" },
+  { tag: "Ruby", percentage: "50%" },
+  { tag: "HTML", percentage: "90%" },
+  { tag: "CSS", percentage: "90%" },
+  { tag: "JS", percentage: "60%" },
+  { tag: "SQL", percentage: "50%" },
+  { tag: "x86 asm", percentage: "20%" },
+];
