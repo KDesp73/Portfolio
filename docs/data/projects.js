a3b64854a39d5b0fa19645d57893c3659de1a162
Konstantinos Despoinidis <kdesp2003@gmail.com>
2023-06-09 01:56:00 +0300

    data from index here

---
+++ docs/data/projects.js
@@ -0,0 +1,31 @@
+const projects = [
+  {
+    title: "Docs Site",
+    link: "https://kdesp73.github.io/Docs/",
+    img: "https://user-images.githubusercontent.com/63654361/215619520-e666a958-6c39-417f-9e14-c702507a10c8.png",
+  },
+
+  {
+    title: "MADB library",
+    link: "https://github.com/KDesp73/MADB",
+    img: "https://www.logo.wine/a/logo/Microsoft_Access/Microsoft_Access-Logo.wine.svg"
+  },
+
+  {
+    title: "Swing-Themes-Lib",
+    link: "https://github.com/KDesp73/Swing-Themes-Library",
+    img: "https://user-images.githubusercontent.com/63654361/218871863-16ae4acc-36ab-4039-9907-198fc741d9ec.jpg"
+  },
+
+  {
+    title: "SyncR",
+    link: "https://github.com/KDesp73/SyncR",
+    img: "https://user-images.githubusercontent.com/63654361/218872414-29d6ddc2-fdac-4157-bccb-b8b4b1fdaaa6.png"
+  },
+
+  {
+    title: "Ruby Web Server",
+    link: "https://github.com/KDesp73/Ruby-Web-Server",
+    img: "https://freesvg.org/img/web_server.png"
+  }
+];
