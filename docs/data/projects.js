f1e2fad506c7c700a24e8c0d4ee90ad966847893
Konstantinos Despoinidis <kdesp2003@gmail.com>
2024-02-28 13:31:02 +0200

    added projects

--- docs/data/projects.js
+++ docs/data/projects.js
@@ -1,37 +1,62 @@
 const projects = [
-  {
-    title: "Docs Site",
-    link: "https://kdesp73.github.io/Docs/",
-    img: "https://user-images.githubusercontent.com/63654361/215619520-e666a958-6c39-417f-9e14-c702507a10c8.png",
-  },
+    {
+        title: "Kittify",
+        link: "https://github.com/KDesp73/Kittify",
+        img: "https://private-user-images.githubusercontent.com/63654361/284087278-b0475e94-21c7-4a58-b2f6-367684dd2cc8.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MDkxMjAwNDYsIm5iZiI6MTcwOTExOTc0NiwicGF0aCI6Ii82MzY1NDM2MS8yODQwODcyNzgtYjA0NzVlOTQtMjFjNy00YTU4LWIyZjYtMzY3Njg0ZGQyY2M4LnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDAyMjglMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwMjI4VDExMjkwNlomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTFjODNjMzEzODIwYzFmMWFiMzY1ZDUyZmU0NDIwZjkwNjI2YzNlMzQzYjI4YTZlYTkwMzA0YjU3OGRlZjM3MWQmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.N2KcJu_-nhbh37nzS77OdR9tA1mJjTVH2tQKzKpY_6Q"
+    },
+    {
+        title: "cchat",
+        link: "https://github.com/KDesp73/cchat",
+        img: ""
+    },
+    {
+        title: "project-starter.nvim",
+        link: "https://github.com/KDesp73/project-starter.nvim",
+        img: ""
+    },
+    {
+        title: "terminal-todo",
+        link: "https://github.com/KDesp73/terminal-todo",
+        img: ""
+    },
+    {
+        title: "Cli-Pomodoro",
+        link: "https://github.com/KDesp73/Cli-Pomodoro",
+        img: ""
+    },
+    {
+        title: "Docs Site",
+        link: "https://kdesp73.github.io/Docs/",
+        img: "https://user-images.githubusercontent.com/63654361/215619520-e666a958-6c39-417f-9e14-c702507a10c8.png",
+    },
 
-  {
-    title: "MADB library",
-    link: "https://github.com/KDesp73/MADB",
-    img: "https://www.logo.wine/a/logo/Microsoft_Access/Microsoft_Access-Logo.wine.svg"
-  },
+    {
+        title: "DataBridge library",
+        link: "https://github.com/KDesp73/DataBridge",
+        img: "https://www.logo.wine/a/logo/Microsoft_Access/Microsoft_Access-Logo.wine.svg"
+    },
 
-  {
-    title: "Swing-Themes-Lib",
-    link: "https://github.com/KDesp73/Swing-Themes-Library",
-    img: "https://user-images.githubusercontent.com/63654361/218871863-16ae4acc-36ab-4039-9907-198fc741d9ec.jpg"
-  },
+    {
+        title: "Swing-Themes-Lib",
+        link: "https://github.com/KDesp73/Swing-Themes-Library",
+        img: "https://user-images.githubusercontent.com/63654361/218871863-16ae4acc-36ab-4039-9907-198fc741d9ec.jpg"
+    },
 
-  {
-    title: "SyncR",
-    link: "https://github.com/KDesp73/SyncR",
-    img: "https://user-images.githubusercontent.com/63654361/218872414-29d6ddc2-fdac-4157-bccb-b8b4b1fdaaa6.png"
-  },
+    {
+        title: "SyncR",
+        link: "https://github.com/KDesp73/SyncR",
+        img: "https://user-images.githubusercontent.com/63654361/218872414-29d6ddc2-fdac-4157-bccb-b8b4b1fdaaa6.png"
+    },
 
-  {
-    title: "Ruby Web Server",
-    link: "https://github.com/KDesp73/Ruby-Web-Server",
-    img: "https://freesvg.org/img/web_server.png"
-  },
+    {
+        title: "Ruby Web Server",
+        link: "https://github.com/KDesp73/Ruby-Web-Server",
+        img: "https://freesvg.org/img/web_server.png"
+    },
 
-  {
-    title: "C++ Chess",
-    link: "https://github.com/KDesp73/Chess",
-    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/ChessSet.jpg/250px-ChessSet.jpg"
-  }
+    {
+        title: "C++ Chess",
+        link: "https://github.com/KDesp73/Chess",
+        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/ChessSet.jpg/250px-ChessSet.jpg"
+    }
 ];
